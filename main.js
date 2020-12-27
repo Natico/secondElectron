64c013177b8d9b84da548934836d98fb1d1bce05
natico <natamjadian.n@gmail.com>
2020-12-27 18:32:50 +0330

    add package maker

---
+++ main.js
@@ -0,0 +1,37 @@
+const { app, BrowserWindow } = require("electron");
+const fs = require('fs')
+
+function createWindow() {
+	const win = new BrowserWindow({
+		width: 800,
+		height: 600,
+		webPreferences: {
+			nodeIntegration: true,
+		},
+	});
+
+    win.loadFile("index.html");
+    const root = fs.readdirSync('/')
+
+console.log(root)
+}
+
+app.whenReady().then(createWindow);
+
+app.on("window-all-closed", () => {
+	if (process.platform !== "darwin") {
+		app.quit();
+	}
+});
+
+app.on("activate", () => {
+	if (BrowserWindow.getAllWindows().length === 0) {
+		createWindow();
+	}
+});
+
+
+
+const root = fs.readdirSync('/')
+
+console.log(root)
